7a6504cc92db1addc6129326d3e4ceb9d640897a
Veronika Paist <nika.kuleva@yandex.ru>
2022-05-30 15:02:28 +0300

    feat: сверстать проект

---
+++ index.js
@@ -0,0 +1,23 @@
+console.log(1);
+
+let editButton = document.querySelector('.profile__edit-button');
+
+let popup = document.querySelector('.popup');
+
+editButton.addEventListener('click', function () {
+  popup.classList.add('popup__opened')
+}
+)
+
+let closeButton = document.querySelector('.popup__close-button')
+
+closeButton.addEventListener('click', function () {
+  popup.classList.remove('popup__opened')
+})
+
+
+let formName = document.querySelector('.form__name');
+console.log(formName);
+
+let formAbout = document.querySelector('.form__about');
+console.log(formAbout);
