b1b386b1b6883c964e80ed769fd7d59e4aa82601
Veronika Paist <nika.kuleva@yandex.ru>
2022-06-03 14:11:20 +0300

    fix:1st review

--- script/index.js
+++ script/index.js
@@ -1,35 +1,32 @@
-console.log(1);
-
 let editButton = document.querySelector('.profile__edit-button');
-
 let popup = document.querySelector('.popup');
 let formElement = document.querySelector('.popup__form');
 let name = document.querySelector('.profile__name');
 let about = document.querySelector('.profile__about');
 let nameInput = formElement.querySelector('.popup__form-name');
 let aboutInput = formElement.querySelector('.popup__form-about');
+let closeButton = document.querySelector('.popup__close-button');
 
 function openPopup() {
-  popup.classList.add('popup__opened');
+  popup.classList.add('popup_opened');
   nameInput.value = name.textContent;
   aboutInput.value = about.textContent;
 }
 
 editButton.addEventListener('click', openPopup);
 
-let closeButton = document.querySelector('.popup__close-button');
+function closePopup() {
+  popup.classList.remove('popup_opened');
+}
 
-closeButton.addEventListener('click', function () {
-  popup.classList.remove('popup__opened')
-});
+
+closeButton.addEventListener('click', closePopup);
 
 function formSubmitHandler(evt) {
   evt.preventDefault();
   name.textContent = nameInput.value;
   about.textContent = aboutInput.value;
+  closePopup();
 }
 
 formElement.addEventListener('submit', formSubmitHandler);
-formElement.addEventListener('submit', function () {
-  popup.classList.remove('popup__opened')
-});
